6f607cb939f1053f968864bfedd29c88385066d8
TheHandsomeGenius <nauyoahz@hotmail.com>
2022-06-04 01:01:24 +0800

    troubleshoot css

--- public/js/index.js
+++ public/js/index.js
@@ -5,13 +5,4 @@ document.querySelector("a").style.color = colours[randPos];
 if (document.body.clientWidth <= 768) {
  document.querySelector(".navbar-brand").style.marginRight = "0";
  document.querySelector(".navbar-brand").style.fontSize = "13px";
-
- if (window.location.pathname == "/") {
-  document.querySelector(".carousel").style.marginTop = "16%";
-  document.querySelector(".carouselPage3 h1").style.margin = "0 0 20px 10px";
-  for (let i=0;i<document.querySelectorAll("td").length;i++) {
-   document.querySelectorAll("td")[i].style.fontSize = "13px";
-  };
-  document.querySelector(".indexSecondDiv h1").style.fontSize = "2rem";
- };
 };
